c06cc542efc310e76ebb2385aee5b53e57e3e1ce
Renzo <w3renzo@gmail.com>
2021-10-06 13:25:01 -0300

    Fetching products working

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,9 @@
-import Head from 'next/head'
+import Head from "next/head";
+import { getProductsInCollection } from "../lib/shopify";
 
-export default function Home() {
+export default function Home({ products }) {
+  console.log(products);
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <Head>
@@ -10,14 +13,14 @@ export default function Home() {
 
       <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
         <h1 className="text-6xl font-bold">
-          Welcome to{' '}
+          Welcome to{" "}
           <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
+            Next.tsx!
           </a>
         </h1>
 
         <p className="mt-3 text-2xl">
-          Get started by editing{' '}
+          Get started by editing{" "}
           <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
             pages/index.js
           </code>
@@ -73,10 +76,18 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
         </a>
       </footer>
     </div>
-  )
+  );
+}
+
+export async function getStaticProps() {
+  const products = await getProductsInCollection();
+
+  return {
+    props: { products },
+  };
 }
